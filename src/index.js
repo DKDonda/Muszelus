41fce70f15a3aacfb40bba6783efc3dd0038a7a0
DKDonda <dkdonda@mun.ca>
2023-09-13 19:25:38 -0230

    new1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./components/store/store";
+import { PersistGate } from "redux-persist/integration/react"; // Import PersistGate
+import store, { persistor } from "./components/store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
